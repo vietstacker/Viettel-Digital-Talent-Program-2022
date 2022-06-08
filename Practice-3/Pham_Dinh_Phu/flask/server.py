7cefc48f9eef8fe6bee9cda50a6a93e762bf3521
phupham1805 <phupham.utc@gmail.com>
2022-06-08 16:03:04 +0700

    update fix

--- Practice-3/Pham_Dinh_Phu/flask/server.py
+++ Practice-3/Pham_Dinh_Phu/flask/server.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+import os
+
 from flask import Flask                     
 from pymongo import MongoClient       
 
@@ -15,4 +18,3 @@ def todo():
 
 if __name__ == '__main__':   
     app.run(host='0.0.0.0', port=os.environ.get("FLASK_SERVER_PORT", 9090), debug=True)     
-    
