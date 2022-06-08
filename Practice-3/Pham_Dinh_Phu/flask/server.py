5725b192805006f0ba9564a0f7b6f2ce1fe302d9
phupham1805 <phupham.utc@gmail.com>
2022-06-08 15:17:04 +0700

    update Project

---
+++ Practice-3/Pham_Dinh_Phu/flask/server.py
@@ -0,0 +1,18 @@
+from flask import Flask                     
+from pymongo import MongoClient       
+
+app = Flask(__name__)        
+
+client = MongoClient("mongo:27017")       
+
+@app.route("/")            
+def todo():  
+    try:   
+        client.admin.command('ismaster')  
+    except:   
+        return "Server not avaiable"   
+    return "Hello from the MongoDB client!\n"   
+
+if __name__ == '__main__':   
+    app.run(host='0.0.0.0', port=os.environ.get("FLASK_SERVER_PORT", 9090), debug=True)     
+    
